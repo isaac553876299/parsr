f4d8619bba88489c0fcb5cb460c91da8be31043c
isaac553876299 <60979696+isaac553876299@users.noreply.github.com>
2021-10-13 14:21:07 +0200

    h

--- parsr/parsr/src/Source.cpp
+++ parsr/parsr/src/Source.cpp
@@ -1,6 +1,17 @@
 #include "parsr.hpp"
 int main()
 {
+	const char arr[] = "-\\|/";
+	int x = 0, y = 0;
+	while (0)
+	{
+		++y;
+		if (y > 99999999)
+		{
+			y = 0;
+			std::cout << '\b' << arr[(x > sizeof(arr) - 1) ? x = 0 : x++];
+		}
+	}
 	parsr_document test("src/test.xml");
 	return 0;
 }
